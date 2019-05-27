e6ae146b1eb768ab4cac31b845cc712a6a05f42a
ehlerorngard <ehlerorngard@gmail.com>
2019-05-27 11:51:44 -0700

    v2.0.1

--- backend/backend/production_settings.py
+++ backend/backend/production_settings.py
@@ -2,16 +2,9 @@ import os
 import psycopg2
 
 
-# CONNECT to the postgres database using a uri and psycopg2:
-DATABASE_URL = os.environ.get('DATABASE_URL', '')
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['orngard.herokuapp.com']
 
 # Database
 DATABASES = {
@@ -20,16 +13,37 @@ DATABASES = {
     }
 }
 
+# CONNECT to the postgres database using a uri and psycopg2:
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
 
-# Static files (CSS, JavaScript, Images)
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+# Use dj_database_url to decipher the DATABASE_URL into a format Django can read
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
+
+
+# Static files (CSS, JavaScript, Images) 
+# (I'm not actually needing to serve static files from the backend.)
 STATIC_URL = 'https://www.orngard.com/'
 
 
 
+# Allow the web server host:
+ALLOWED_HOSTS = ['orngard.herokuapp.com']
 
-# Ensures the CSRF cookies is sent from a secure (https) location
-CSRF_COOKIE_SECURE = True
+"""
+As of v3.0.0 (May 2019) whitelisted CORS origins must include 
+a scheme ('http://' or 'https://') as well as hostname:
+"""
+CORS_ORIGIN_WHITELIST = ['https://orngard.com', 'http://localhost:3000', 'https://www.orngard.com']
 
+CSRF_TRUSTED_ORIGINS = ['localhost:3000', 'www.orngard.com', 'orngard.com']
+
+# Allow these headers on the request
 CORS_ALLOW_HEADERS = [
     'accept',
     'access-control-request-headers',
@@ -55,44 +69,39 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
-
-# to decipher the DATABASE_URL into a format Django can read
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-
-# added due to SECURITY WARNINGS:
-
+# Allow cross origin cookies:
 SESSION_COOKIE_SAMESITE = None
 CRSF_COOKIE_SAMESITE = None
 
 
-SECURE_CONTENT_TYPE_NOSNIFF = False
-SESSION_COOKIE_SECURE = False
-SECURE_BROWSER_XSS_FILTER = False
-SECURE_SSL_REDIRECT = False
-X_FRAME_OPTIONS = 'DENY'
+"""
+________[ NOTE ]________
+Many of the following are synonymous with the defaults for those settings,
+but I like clarity and prefer minimizing ambiguity regarding things that
+impact critical functionality, so I am including them for easy future reference.
+"""
 
-# SECURE_HSTS_SECONDS = 60
+# Ensure the CSRF cookie is sent from a secure (https) location:
+CSRF_COOKIE_SECURE = True
 
-CSRF_USE_SESSIONS = False
-CSRF_COOKIE_HTTPONLY = False
-CSRF_COOKIE_NAME = 'csrftoken'
-
-# CSRF_COOKIE_DOMAIN = 'orngard.com' # don't need with CSRF_TRUSTED_ORIGINS
-
-
-# Don't need since I'm not using cookies for authentication:
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
+CSRF_COOKIE_NAME = 'csrftoken'
 
+CSRF_USE_SESSIONS = False
 
-"""
-As of v3.0.0 (May 2019) whitelisted CORS origins must include 
-a scheme ('http://' or 'https://') as well as hostname:
-"""
-CORS_ORIGIN_WHITELIST = ['https://orngard.com', 'http://localhost:3000', 'https://www.orngard.com']
-# CORS_ORIGIN_ALLOW_ALL = True
+# True would disallow csrf cookies in a response (anything other than in HTTP)
+CSRF_COOKIE_HTTPONLY = False
 
-CSRF_TRUSTED_ORIGINS = ['localhost:3000', 'www.orngard.com', 'orngard.com']
+# Only send session cookies to https:
+SESSION_COOKIE_SECURE = False
+
+SECURE_CONTENT_TYPE_NOSNIFF = False
+
+# Enable XSS filter in the browser, and force it to always block suspected XSS attacks:
+SECURE_BROWSER_XSS_FILTER = True
+
+SECURE_SSL_REDIRECT = False
+
+X_FRAME_OPTIONS = 'DENY'
