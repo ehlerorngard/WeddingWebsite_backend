3847e783cacdbda5326b39c385d9791745f2ff65
ehlerorngard <ehlerorngard@gmail.com>
2019-05-26 19:13:24 -0700

    allow all

--- backend/backend/production_settings.py
+++ backend/backend/production_settings.py
@@ -32,7 +32,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 # Ensures the CSRF cookies is sent from a secure (https) location
-CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
 
 CORS_ALLOW_HEADERS = [
     'accept',
@@ -73,11 +73,13 @@ SESSION_COOKIE_SECURE = False
 SECURE_BROWSER_XSS_FILTER = False
 SECURE_SSL_REDIRECT = False
 X_FRAME_OPTIONS = 'DENY'
+
 # SECURE_HSTS_SECONDS = 60
 
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_HTTPONLY = False
 CSRF_COOKIE_NAME = 'csrftoken'
+
 # CSRF_COOKIE_DOMAIN = 'orngard.com' # don't need with CSRF_TRUSTED_ORIGINS
 
 
@@ -91,7 +93,7 @@ CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
 As of v3.0.0 (May 2019) whitelisted CORS origins must include 
 a scheme ('http://' or 'https://') as well as hostname:
 """
-CORS_ORIGIN_WHITELIST = ['https://orngard.com', 'http://localhost:3000']
-# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = ['https://orngard.com', 'http://localhost:3000']
+CORS_ORIGIN_ALLOW_ALL = True
 
 CSRF_TRUSTED_ORIGINS = ['localhost:3000', 'www.orngard.com']
